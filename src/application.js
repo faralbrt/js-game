4e1e4163b8e70676fec5fec9bb91dc2a9f016ae5
faralbrt <albert.farhi5@gmail.com>
2016-12-23 04:06:50 -0500

    add logic for unavailable moves

--- src/application.js
+++ src/application.js
@@ -5,19 +5,29 @@ $(document).ready(function()  {
   playerTwo.loadCheckers();
   refreshBoard(playerOne, playerTwo);
   $( function() {
-    dropPoints();
-    $(".draggable").draggable({containment: "#board",scroll: false, revert: "invalid", cursor: "move" });
-    $(".droppable").droppable({ drop: function( event, ui ) {
-      var oldPosition = $("tr").index(ui.draggable.parent().parent()) + 1;
-      var newPosition = $("tr").index($(this).parent().parent()) + 1;
-      var moves = Math.abs(oldPosition - newPosition);
-      var item = ui.draggable.remove();
-      var td = $(this).parent();
-      item.css("position", "static");
-      td.prepend(item);
-      if(ui.draggable.attr('class').includes("black")) {playerOne.moveChecker(oldPosition, moves);}
-      else if(ui.draggable.attr('class').includes("red")) {playerTwo.moveChecker(oldPosition, moves);}
-      refreshBoard(playerOne, playerTwo);
-    }});
+    $(".draggable").draggable({
+      containment: "#board",
+      scroll: false,
+      revert: "invalid",
+      cursor: "move",
+      start: function(event) {
+        if($(event.target).attr('class').includes("black")) {
+          dropPoints({opponent: playerTwo});
+        }
+      }
+    });
   });
+  $(".droppable").droppable({
+    drop: function( event, ui ) {
+    var oldPosition = $("tr").index(ui.draggable.parent().parent()) + 1;
+    var newPosition = $("tr").index($(this).parent().parent()) + 1;
+    var moves = Math.abs(oldPosition - newPosition);
+    var item = ui.draggable.remove();
+    var td = $(this).parent();
+    item.css("position", "static");
+    td.prepend(item);
+    if(ui.draggable.attr('class').includes("black")) {playerOne.moveChecker(oldPosition, moves);}
+    else if(ui.draggable.attr('class').includes("red")) {playerTwo.moveChecker(oldPosition, moves);}
+    refreshBoard(playerOne, playerTwo);
+  }});
 });
--- src/view.js
+++ src/view.js
@@ -16,9 +16,24 @@ function insertPiece(checker) {
   div.find("div").addClass("draggable");
 }
 
-function dropPoints() {
-  var div = "<div class=\"droppable\" style=\"position: absolute; bottom: 0; right: 0; width: 20px;height: 175px; margin: 0px; padding: 0px; border: 2px solid black; z-index: -1;\"></div>";
-  $("td").slice(0,12).prepend(div);
-  div = "<div class=\"droppable\" style=\"position: absolute; top: 0; left: 0; width: 20px;height: 175px; margin: 0px; padding: 0px; border: 2px solid black; z-index: -1;\"></div>";
-  $("td").slice(12).prepend(div);
+function dropPoints(args) {
+  var opponent = args.opponent;
+  var opponentPoints = opponent.checkers.map(function(checker) {return checker.position;});
+  for(var i = 0; i < 25; i ++) {
+    if (!opponentPoints.includes(i)) {
+      dropPoint(i);
+    }
+  }
+}
+
+function dropPoint(point) {
+  var div;
+  if(point <= 12 && point > 0) {
+    div = "<div class=\"droppable\" style=\"position: absolute; bottom: 0; right: 0; width: 20px;height: 175px; margin: 0px; padding: 0px; border: 2px solid black; z-index: -1;\"></div>";
+    $("tbody tr:nth-child("+ point +")").prepend(div);
+  }
+  else if(point > 13 && point < 25) {
+    div = "<div class=\"droppable\" style=\"position: absolute; top: 0; left: 0; width: 20px;height: 175px; margin: 0px; padding: 0px; border: 2px solid black; z-index: -1;\"></div>";
+    $("tbody tr:nth-child("+ point +")").prepend(div);
+  }
 }
