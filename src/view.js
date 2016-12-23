30a6a2748ee3ff47c192712eb800d6c1b1eb0123
faralbrt <albert.farhi5@gmail.com>
2016-12-23 02:19:47 -0500

    add droppable points and drop works

--- src/view.js
+++ src/view.js
@@ -4,9 +4,16 @@ function refreshBoard(playerOne, playerTwo) {
   totalCheckers.forEach(function(checker) {
     insertPiece(checker);
   });
+  dropPoints();
   $( function() {
-   $(".draggable").draggable({containment: ".board",revert: "invalid", cursor: "move" });
-   $(".droppable").droppable({});
+   $(".draggable").draggable({containment: "#board",scroll: false, revert: "invalid", cursor: "move" });
+   $(".droppable").droppable({ drop: function( event, ui ) {
+        var originalPosition = $("tr").index(ui.draggable.offsetParent()) + 1;
+        var item = ui.draggable.remove();
+        var td = $(this).parent();
+        item.css("position", "static");
+        td.prepend(item);
+      }});
   });
 }
 
@@ -16,4 +23,11 @@ function insertPiece(checker) {
   var position = checker.position;
   var div = $("tbody tr:nth-child("+ position +")").children().append("<div class=" + color  +  "></div>");
   div.find("div").addClass("draggable");
+}
+
+function dropPoints() {
+  var div = "<div class=\"droppable\" style=\"position: absolute; bottom: 0; right: 0; width: 20px;height: 175px; margin: 0px; padding: 0px; border: 2px solid black; z-index: -1;\"></div>";
+  $("td").slice(0,12).prepend(div);
+  div = "<div class=\"droppable\" style=\"position: absolute; top: 0; left: 0; width: 20px;height: 175px; margin: 0px; padding: 0px; border: 2px solid black; z-index: -1;\"></div>";
+  $("td").slice(12).prepend(div);
 }
